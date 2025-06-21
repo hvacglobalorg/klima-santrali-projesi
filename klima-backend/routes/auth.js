72aa6d362e57ab43b31b6c0f43fe8632ee84d551
hvacglobalorg <hvacglobalorg@gmail.com>
2025-06-21 12:18:45 +0300

    Fix: Token içine username eklendi

--- klima-backend/routes/auth.js
+++ klima-backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const verifyToken = require('../middleware/verifyToken');
 const User = require('../models/User');  // Modelin doğru yerde olduğundan emin ol
 
 // Kullanıcı kayıt endpoint'i
